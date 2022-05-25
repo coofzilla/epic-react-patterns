20a77da2257225d2b9536c3ceee83fb5810deb1d
coofzilla <jeric614@yahoo.com>
2022-05-25 18:49:15 +0900

    get toggler props w/custom onclick

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -8,23 +8,42 @@ function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
+  const callAll =
+    (...fns) =>
+    (...args) =>
+      fns.forEach(fn => fn?.(...args))
+
+  const getTogglerProps = ({onClick, ...props} = {}) => {
+    return {
+      'aria-pressed': on,
+      onClick: callAll(onClick, toggle),
+      ...props,
+    }
+  }
+
   return {
     on,
     toggle,
-    togglerProps: {
-      'aria-pressed': on,
-      onClick: toggle,
-    },
+    getTogglerProps,
   }
 }
 
 function App() {
-  const {on, togglerProps} = useToggle()
+  const {on, getTogglerProps} = useToggle()
   return (
     <div>
-      <Switch on={on} {...togglerProps} />
+      <Switch {...getTogglerProps({on})} />
       <hr />
-      <button aria-label="custom-button" {...togglerProps}>
+      <button
+        {...getTogglerProps({
+          'aria-label': 'custom-button',
+          onClick: () => console.info('onButtonClick'),
+          id: 'custom-button-id',
+        })}
+        // aria-label="custom-button"
+        // {...togglerProps}
+        // onClick={() => console.log('onButtonClick')}
+      >
         {on ? 'on' : 'off'}
       </button>
     </div>
